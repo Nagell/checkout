020c0a3a2eeefc910d40376e5be11ecba888302f
Dawid Nitka <dawidnitka@gmail.com>
2020-09-06 16:11:58 +0200

    rework to TS tmp

---
+++ src/types/index.ts
@@ -0,0 +1,25 @@
+export interface Tab {
+    id: number
+    name: string
+    icon: string
+    toPay: number
+}
+
+export interface Result {
+    payment: number
+    difference: number
+    banknotesAmount: number
+    payments: string
+}
+
+export interface PrepareResult {
+    targetAmount: number
+    sum: number
+    payments: number[]
+}
+
+export interface ReducedResult {
+    payment: number
+    difference: number
+    banknotesAmount: number
+}
